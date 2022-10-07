5ea85d95e672108efce2049970dce541d332390a
okputu julius <juliusstan10@gmail.com>
2022-10-07 23:14:08 +0100

    omorr

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ burger.addEventListener('click', (e) => {
     menu.classList.add('hidden');
   }
 })
+
+
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
