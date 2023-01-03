60709ce6fe867116143db741f8a152b1801d1dab
okputu julius <juliusstan10@gmail.com>
2023-01-03 10:06:32 +0100

    add error handling block
    add if statement in index.js to check if hamburger is null

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 
-burger.addEventListener('click', (e) => {
-  if (menu.classList.contains('hidden')) {
-    menu.classList.remove('hidden');
-  } else {
-    menu.classList.add('hidden');
-  }
-})
+if (burger !== null)
+{
+  burger.addEventListener('click', (e) => {
+    if (menu.classList.contains('hidden')) {
+      menu.classList.remove('hidden');
+    } else {
+      menu.classList.add('hidden');
+    }
+  })
+
+}
+
 
 
 
